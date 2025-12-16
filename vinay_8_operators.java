d0f0abc5d4b733471820112ff913f550f603bf9e
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2025-12-16 20:32:06 +0530

    Add files via upload

---
+++ vinay_8_operators.java
@@ -0,0 +1,62 @@
+public class vinay_8_operators {
+    static void main() {
+        int a = 4;
+// // ----- arithmetic operator -----
+
+        int b = 6 + a;
+//        System.out.println(b);
+//        int c = 6 - a;
+//        System.out.println(c);
+//        int d = 6 * a;
+//        System.out.println(d);
+//        int e = 6 % a;
+//        System.out.println(e);
+//        int f = 6 / a;
+//        System.out.println(f);
+
+//      //  increment
+//        a++;
+//        int g = a;
+//        System.out.println(g);
+
+//      //  decrement
+//        a--;
+//        int h = a;
+//        System.out.println(h);
+
+// // // ----- Assignment operator -----
+//        int c = 2;
+// //        c += 10;
+//        c *= 10;
+//        System.out.println(c);
+
+// // // ----- Comparison operator -----
+//        System.out.println(5==6);
+//        System.out.println(5==5);
+//
+//        System.out.println(5<5);
+//        System.out.println(5<6);
+//
+//        System.out.println(5<=5);
+//        System.out.println(5<=6);
+//
+//        System.out.println(5>=5);
+//        System.out.println(5>=6);
+
+// // ----- Logical operator -----
+
+//        System.out.println(5<=5 && 5>=5 );
+//        System.out.println(5<5 && 5>=5 );
+
+//        System.out.println(5<=5 || 5>=5 );
+//        System.out.println(5<5 || 5>=5 );
+
+//
+//        System.out.println(!(5<=5 || 5>=5 ));
+//        System.out.println(!(5<5 || 5>=5 ));
+
+//        Bitwise operator
+//        System.out.println(2|3);
+        System.out.println(5&3);
+    }
+}
