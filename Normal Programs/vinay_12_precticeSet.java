988529bff200fc1969485a7f9ddfe82b8acfdb70
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2025-12-24 11:05:54 +0530

    Add files via upload

---
+++ Normal Programs/vinay_12_precticeSet.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class vinay_12_precticeSet {
+    static void main() {
+//        // Question - 1
+//        float a = 7/4.0f * 9/2.0f;
+//        System.out.println(a);
+
+//        // Question - 2
+//        char grade = 'B';
+//        grade = (char)(grade+8) ;
+//        System.out.println(grade);
+//
+//        grade = (char)(grade-8);
+//        System.out.println(grade);
+
+//        // Question - 3
+//        System.out.println("Enter the integer number ");
+//        Scanner UserInput = new Scanner(System.in);
+//        double u = UserInput.nextDouble();
+//        System.out.println(u>8);
+
+        // // Question - 4
+//        int v = 5;
+//        int U = 6;
+//        int a = 7;
+//        int s = 8;
+//        double us = (v*v - U*U)/2*a*s;
+//        System.out.println(us);
+
+        // // Question - 5
+        System.out.println(7*49/7 + 35/7);
+    }
+}
